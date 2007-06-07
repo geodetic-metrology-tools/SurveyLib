5bb49d2e8becc241b2f89413222b0f001722f167
jhefferm <jhefferm@cern.ch>
2007-06-07 13:27:32 +0000

    Compiled with 2003 - Without the changes made for the Pocket PC DLL

--- ProjectFramework/ProjectData/TDataParameters.cpp
+++ ProjectFramework/ProjectData/TDataParameters.cpp
@@ -275,6 +275,49 @@ void  TDataParameters::setLengthPrecision(const TObservationFormat::ELengthPreci
 	return; 
 }
 
+void TDataParameters::setPrecision(int precision)
+{
+
+		TObservationFormat::ELengthPrecision observationPrecision;
+		TPointFormat::ECoordPrecision pointPrecision;
+
+		//Should be an integer between 0 (m) and 7 (100nm)
+		switch(precision){
+	
+		case 0:
+			observationPrecision = TObservationFormat::kMetre;
+			pointPrecision = TPointFormat::kMetre; break;
+		case 1:
+			observationPrecision = TObservationFormat::k100Millimetres; 
+			pointPrecision = TPointFormat::k100Millimetres; break;
+		case 2:
+			observationPrecision = TObservationFormat::k10Millimetres; 
+			pointPrecision = TPointFormat::k10Millimetres; break;
+		case 3:
+			observationPrecision = TObservationFormat::kMillimetre; 
+			pointPrecision = TPointFormat::kMillimetre; break;
+		case 4:
+			observationPrecision = TObservationFormat::k100Micrometres; 
+			pointPrecision = TPointFormat::k100Micrometres; break;
+		case 5:
+			observationPrecision = TObservationFormat::k10Micrometres;
+			pointPrecision = TPointFormat::k10Micrometres; break;
+		case 6:
+			observationPrecision = TObservationFormat::kMicrometre;
+			pointPrecision = TPointFormat::kMicrometre; break;
+		case 7:
+			observationPrecision = TObservationFormat::k100Nanometres;
+			pointPrecision = TPointFormat::k100Nanometres; break;
+		default:
+			observationPrecision = TObservationFormat::k10Micrometres; 
+			pointPrecision = TPointFormat::k10Micrometres; break;
+
+		}
+
+		setCoordPrecision(pointPrecision); 
+		setLengthPrecision(observationPrecision);
+		setAnglePrecision(TObservationFormat::k10Microgons);
+}
 
 //! set point name's width precision 
 void  TDataParameters::setPointNameWidth(const int width )  
@@ -474,6 +517,7 @@ TPointFormat::ECoordPrecision  TDataParameters::getCoordPrecision() const
 {//! get the coord precision 
 	return fCoordPrecision;
 }
+
 
 
 int  TDataParameters::getPointNameWidth() const
