c34b066d4e819bb3314bfba0610df201f14a188f
bpetrov <bpetrov@cern.ch>
2009-11-24 10:26:42 +0000

    Merged the calc params into a single template class

--- LSAlgorithms/CalcParams/TLSCalcWorkingPosVec.cpp
+++ LSAlgorithms/CalcParams/TLSCalcWorkingPosVec.cpp
@@ -2,117 +2,34 @@
 
 #include "TLSCalcWorkingPosVec.h"
 
-//////////////////////////////////////////////////////////
-// CONSTRUCTOR / DESTRUCTOR
-//////////////////////////////////////////////////////////
-TLSCalcWorkingPosVec::TLSCalcWorkingPosVec(): fLastElNbr(0),fListError(false)
-{// Default constructor 
-	fLSPosVectorList.clear();
-}
-
-
-TLSCalcWorkingPosVec::~TLSCalcWorkingPosVec()
-{//Destructor 
-	fLSPosVectorList.clear();
-}
-
-
 ////////////////////////////////////////////////////////////////
 // PUBLIC METHODS		
 ////////////////////////////////////////////////////////////////
-LSPosVecIter	TLSCalcWorkingPosVec::insert(LSPosVecIter it, TLSCalcPosVectorParam& posVec)
+LSPosVecIter TLSCalcWorkingPosVec::insert(LSPosVecIter it, TLSCalcPosVectorParam& posVec)
 {
-	hash_map<string, LSPosVecIter>::iterator iter = posVectorMap.find(posVec.getName());
+	hash_map<string, LSPosVecIter>::iterator iter = calcParamsMap.find(posVec.getName());
 	LSPosVecIter lsPVIter;
 
-	if(!fListError)
+	// if the observation isn't in the container -> insert
+	if (iter == calcParamsMap.end())
 	{
-		// if the observation isn't in the container -> insert
-		if (iter == posVectorMap.end())
-		{
-			lsPVIter = fLSPosVectorList.insert(it, posVec);
-			posVectorMap[posVec.getName()] = lsPVIter;
-			fLastElNbr++;
-		}
-		else
-		{
-			lsPVIter = iter->second;
-		}
+		lsPVIter = paramsList.insert(it, posVec);
+		calcParamsMap[posVec.getName()] = lsPVIter;
 	}
 	else
 	{
-		lsPVIter = end();
+		lsPVIter = iter->second;
 	}
 	
 	return lsPVIter; 
 }
 
-
-LSPosVecIter	TLSCalcWorkingPosVec::push_back(TLSCalcPosVectorParam& posVec)
-{// Inserts a new TLSCalcPosVectorParam in the list, 
-	//if it is not done yet (tests first if the object is already in the list or not)
-
-	hash_map<string, LSPosVecIter>::iterator iter = posVectorMap.find(posVec.getName());
-	LSPosVecIter lsPVIter;
-
-	if(!fListError)
-	{
-		// if the observation isn't in the container -> insert
-		if (iter == posVectorMap.end())
-		{
-			fLSPosVectorList.push_back(posVec);
-			fLastElNbr++;
-			// Debugging for the case where the push_back method fails
-			if(fLastElNbr == fLSPosVectorList.size())
-			{
-				//no error
-				lsPVIter = --(fLSPosVectorList.end());
-			}
-			else
-			{
-				//error
-				//treat error
-				fListError = true;
-				lsPVIter = end();
-			}
-			posVectorMap[posVec.getName()] = lsPVIter;
-		}
-		else
-		{
-			lsPVIter = iter->second;
-		}
-	}
-	else
-	{
-		lsPVIter = end();
-	}
-		
-	return lsPVIter; 
-}
-
-bool TLSCalcWorkingPosVec::erase(TLSCalcPosVectorParam& posVec)
-{// Erases the selected TLSCalcPosVectorParam and 
-	//deletes its corresponding pointer from the list if it exists
-
-	hash_map<string, LSPosVecIter>::iterator iter = posVectorMap.find(posVec.getName());
-
-	if (iter == posVectorMap.end())
-	{
-		return false;
-	}
-	fLSPosVectorList.erase(iter->second);
-	posVectorMap.erase(posVec.getName());
-		
-	return true;
-}
-	
-
-LSPosVecIter	TLSCalcWorkingPosVec::getPoint(const string name)
+LSPosVecIter TLSCalcWorkingPosVec::getPoint(const string name)
 {//returns an iterator on a point, given its Name
-	hash_map<string, LSPosVecIter>::iterator iter = posVectorMap.find(name);
-	if (iter == posVectorMap.end())
+	hash_map<string, LSPosVecIter>::iterator iter = calcParamsMap.find(name);
+	if (iter == calcParamsMap.end())
 	{
-		return fLSPosVectorList.end();
+		return paramsList.end();
 	}
 
 	return iter->second;
@@ -121,10 +38,10 @@ LSPosVecIter	TLSCalcWorkingPosVec::getPoint(const string name)
 
 LSPosVecConstIter	TLSCalcWorkingPosVec::getPoint(const string name) const
 {//returns a const iterator on a point, given its Name
-	hash_map<string, LSPosVecIter>::const_iterator iter = posVectorMap.find(name);
-	if (iter == posVectorMap.end())
+	hash_map<string, LSPosVecIter>::const_iterator iter = calcParamsMap.find(name);
+	if (iter == calcParamsMap.end())
 	{
-		return fLSPosVectorList.end();
+		return paramsList.end();
 	}
 
 	return iter->second;
