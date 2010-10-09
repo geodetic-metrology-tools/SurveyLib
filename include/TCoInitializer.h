eaddfb285130b7f93d79f47774fefcced5b9439a
jhulboj <jhulboj@cern.ch>
2010-10-09 17:25:33 +0000

    [SpatialObject] ADDED: Managing Reframe DLL, added RAII functionality which allows to correctly free the library (call CoUninitialize when necessary).

---
+++ include/TCoInitializer.h
@@ -0,0 +1,26 @@
+class CCoInitializer
+{
+public:
+   explicit CCoInitializer(DWORD dwCoInit = COINIT_APARTMENTTHREADED)
+      : _coinitialized(false)
+   {
+      // Initialize the COM library on the current thread.
+      HRESULT hr = CoInitializeEx(NULL, dwCoInit);
+      if (FAILED(hr))
+         throw hr;
+      _coinitialized = true;
+   }
+   ~CCoInitializer()
+   {
+      // Free the COM library.
+      if (_coinitialized)
+         CoUninitialize();
+   }
+private:
+   // Flags whether COM was properly initialized.
+   bool _coinitialized;
+
+   // Hide copy constructor and assignment operator.
+   CCoInitializer(const CCoInitializer&);
+   CCoInitializer& operator=(const CCoInitializer&);
+};
