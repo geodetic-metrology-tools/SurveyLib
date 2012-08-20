163a6a0747a3aacefbbaedfd5647768f7b972015
jhulboj <jhulboj@cern.ch>
2012-08-20 08:06:19 +0000

    [LGC] ADDED: Introduction of Eigen 3.1.1 library to use sparse matrices.

--- include/SpatialObjFns.h
+++ include/SpatialObjFns.h
@@ -21,12 +21,14 @@ public:
 
 	/// return the (XYZ) position in MLA of a point from the origin of the MLA 
 	/// and the point to be transformed both in CCS
+	//???
 	static int transformToMLA(double x0, double y0, double z0,
 							  double* x, double* y, double* z, char* geoid);
 
 	
 	/// return the (XYZ) position in CCS of a point from the origin of the MLA system
 	/// in CCS and the point to be transformed in MLA 
+	//???@
 	static int transformFromMLA(double x0, double y0, double z0,
 								double* x, double* y, double* z, char* geoid);
 
@@ -34,6 +36,7 @@ public:
 	/// return the (XYZ) position in MLA of a point from the origin of the MLA 
 	/// and the point to be transformed both in CCS (with slope and bearing equal to 0
 	/// and false origin (0,0,0) )
+	//???
 	static int transformToMLA2(double x0, double y0, double z0,
 							   double* x, double* y, double* z, char* geoid);
 
@@ -41,6 +44,7 @@ public:
 	/// return the (XYZ) position in CCS of a point from the origin of the MLA system
 	/// in CCS and the point to be transformed in MLA (with slope and bearing equal to 0
 	/// and false origin (0,0,0) )
+	//???@
 	static int transformFromMLA2(double x0, double y0, double z0,
 								 double* x, double* y, double* z, char* geoid);
 
--- source/SpatialObjects/SurveyLibDllTest/testSurveyLibDLL.cpp
+++ source/SpatialObjects/SurveyLibDllTest/testSurveyLibDLL.cpp
@@ -21,7 +21,7 @@ namespace tut
 
 namespace tut
 {
-    tut::factory tf("Test of csgeoDLL");
+    tut::factory tf("Test of SurveyLibDLL");
 }
 
 namespace tut
