e294d835bb79ee05031191c278b21eea17d22f97
swaniore <swaniore@cern.ch>
2012-12-19 16:07:39 +0000

    surveylib: Added function to version-header that returns a properly formatted version string

--- include/SurveryLibVersion.h
+++ include/SurveryLibVersion.h
@@ -1,8 +1,17 @@
 #ifndef SURVEYLIB_VERSION_H
 #define SURVEYLIB_VERSION_H
 
+#include <string>
+#include <cstdio>
+
 #define SurveyLib_VERSION_MAJOR 3
 #define SurveyLib_VERSION_MINOR 2
 #define SurveyLib_VERSION_PATCH 0
+
+static std::string getSurveyLibVersion(void) {
+	static char str[128];
+	_snprintf(str, 128, "v%d.%02d.%02d", SurveyLib_VERSION_MAJOR, SurveyLib_VERSION_MINOR, SurveyLib_VERSION_PATCH);
+	return std::string(str);
+}
 
 #endif
