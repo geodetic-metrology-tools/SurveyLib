132d869fcff06e7ca7a5e43379478b4915fd62d9
swaniore <swaniore@cern.ch>
2013-06-13 07:46:07 +0000

    surveylib: Updated SurveryLibVersion.h

--- include/SurveryLibVersion.h
+++ include/SurveryLibVersion.h
@@ -6,7 +6,7 @@
 
 #define SurveyLib_VERSION_MAJOR 3
 #define SurveyLib_VERSION_MINOR 3
-#define SurveyLib_VERSION_PATCH 2
+#define SurveyLib_VERSION_PATCH 3
 
 static std::string getSurveyLibVersion(void) {
 	static char str[128];
