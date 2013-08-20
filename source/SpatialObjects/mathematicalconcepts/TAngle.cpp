3c7d2d1e2a8af3b935473cb8adbf43a073733d5c
swaniore <swaniore@cern.ch>
2013-08-20 08:08:50 +0000

    surveylib: Made recent changes in TAngle work with VS2010

--- source/SpatialObjects/mathematicalconcepts/TAngle.cpp
+++ source/SpatialObjects/mathematicalconcepts/TAngle.cpp
@@ -79,8 +79,9 @@ TAngle::TAngle(const TAngle& angle)
 void TAngle::normaliseAngle()
 {
 	// impropable angle, throw.
+	long double val = fValue;
 	if (fabsq(fValue) > 1000*M_PI)
-		throw std::runtime_error("Impropable angle occured: " + to_string(fValue) + "rad.");
+		throw std::runtime_error("Impropable angle occured: " + to_string(val) + "rad.");
 
 	//modification du 08/05/2003 pour le calcul les angles seront exprimes entre -pi et pi
     while (fValue > M_PI - seuil())
