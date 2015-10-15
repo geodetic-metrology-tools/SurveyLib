2a582d7459a59eb87b8db8dcb2cba526ecfb4a02
Quentin Dorleat <quentin.dorleat@cern.ch>
2015-10-15 15:05:53 +0000

    cmake arrangement

--- source/SourceSurveyLib.cmake
+++ source/SourceSurveyLib.cmake
@@ -8,11 +8,11 @@ INCLUDE(${SURVEYLIB_ROOT}/source/SubLibraries.cmake)
 find_package(Doxygen)
 if(DOXYGEN_FOUND)
 	configure_file(${SURVEYLIB_ROOT}/source/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-	add_custom_target(doc
-	${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	COMMENT "Generating API documentation with Doxygen" VERBATIM
-)
+	add_custom_target(doc_survey_lib
+		${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		COMMENT "Generating API documentation with Doxygen" VERBATIM
+	)
 endif(DOXYGEN_FOUND)
 
 
