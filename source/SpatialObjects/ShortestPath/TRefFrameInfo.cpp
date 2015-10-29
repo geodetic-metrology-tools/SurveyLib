6be11f06184abc43c33001cd8f87786678848b40
Quentin Dorleat <quentin.dorleat@cern.ch>
2015-10-29 15:21:14 +0000

    Correct bug I've introduced in last commit

--- source/SpatialObjects/ShortestPath/TRefFrameInfo.cpp
+++ source/SpatialObjects/ShortestPath/TRefFrameInfo.cpp
@@ -200,11 +200,12 @@ TCoordSysFactory::ECoordSys TRefFrameInfo::getDefaultCoordSys(int frame)
 
 bool TRefFrameInfo::isCoordSysAllowed(int frame, TCoordSysFactory::ECoordSys sys)
 {
-    MappingType::const_iterator iter = getMapping().find(static_cast<TRefSystemFactory::ERefFrame>(frame));
-	if(iter!=getMapping().end())
-		return (iter->second.fAllowedCoordSys != 0 && sys != 0);
+   MappingType::const_iterator iter = getMapping().find(static_cast<TRefSystemFactory::ERefFrame>(frame));
+	
+   if(iter!=getMapping().end())
+		return ( (iter->second.fAllowedCoordSys&sys) > 0);
 
-	throw std::invalid_argument("Unknown ERefFrame value");
+   throw std::invalid_argument("Unknown ERefFrame value");
 }
 
 int TRefFrameInfo::getAllowedCoordSysCount(int frame)
