a3f85b344cf82992b5bfa62780411ef4f75dca4b
Marie Barbier <marie.barbier@cern.ch>
2016-03-03 10:44:19 +0000

    delete some unused comments in the dll class

--- source/SpatialObjDLL/SpatialObjCFns.h
+++ source/SpatialObjDLL/SpatialObjCFns.h
@@ -23,6 +23,8 @@ extern "C" {  // only need to export C interface if
 	SU_DECLSPEC int DLLAPI getCG1985N0(double x, double y, double* N);
 	SU_DECLSPEC int DLLAPI getCG1985NMachine(double x, double y, double* N);
 
+	SU_DECLSPEC int DLLAPI getCGSphereN(double x, double y, double* N);
+
 
 	/// return the (XYZ) position in MLA of a point from the origin of the MLA 
 	/// and the point to be transformed both in CCS
@@ -34,21 +36,6 @@ extern "C" {  // only need to export C interface if
 	/// in CCS and the point to be transformed in MLA 
 	SU_DECLSPEC int DLLAPI transformFromMLA(double x0, double y0, double z0,
 									  double* x, double* y, double* z, char* geoid);
-
-
-	/// return the (XYZ) position in MLA of a point from the origin of the MLA 
-	/// and the point to be transformed both in CCS (with slope and bearing equal to 0
-	/// and false origin (0,0,0) )
-	//SU_DECLSPEC int DLLAPI transformToMLA2(double x0, double y0, double z0,
-	//								double* x, double* y, double* z, char* geoid);
-
-
-	/// return the (XYZ) position in CCS of a point from the origin of the MLA system
-	/// in CCS and the point to be transformed in MLA (with slope and bearing equal to 0
-	/// and false origin (0,0,0) )
-	//SU_DECLSPEC int DLLAPI transformFromMLA2(double x0, double y0, double z0,
-	//								  double* x, double* y, double* z, char* geoid);
-
 
 	/// return coordinates as X,Y,h of a point at H metres on the vertical of another point
 	SU_DECLSPEC int DLLAPI descenteVert(double x, double y, double h, double deltaH,
