62f53054e1272c2f4a663164b19e14090b5d8de1
Rémi Ducceschi <remi.ducceschi@gmail.com>
2017-11-03 14:44:19 +0100

    CMake: Use new version of CMake
    - use of the repo SUSoftCMakeCommon
    - update CMake files so everything build
    - update Doxyfile to put the name of the prject
    - add a gitignore for the build folder

--- source/SourceSurveyLib.cmake
+++ source/SourceSurveyLib.cmake
@@ -1,6 +1,3 @@
-# We need some CMake macros
-INCLUDE(CheckIncludeFileCXX)
-
 IF(CMAKE_PROJECT_NAME STREQUAL "SurveyLib")
 	SET(SURVEYLIB_ROOT "${CMAKE_SOURCE_DIR}/..")
 	message("-- Building Surveylib in tree.")
@@ -9,39 +6,13 @@ ENDIF()
 # Inlcude sublibraries
 INCLUDE(${SURVEYLIB_ROOT}/source/SubLibraries.cmake)
 
-# add a target to generate API documentation with Doxygen
-find_package(Doxygen)
-if(DOXYGEN_FOUND)
-	configure_file(${SURVEYLIB_ROOT}/source/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-	add_custom_target(doc_surveylib
-	${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	COMMENT "Generating API documentation with Doxygen" VERBATIM
-)
-endif(DOXYGEN_FOUND)
-
 # Compiler options
 IF(WIN32)
     IF(MSVC)
         ADD_DEFINITIONS(-D_SCL_SECURE_NO_WARNINGS)
         ADD_DEFINITIONS(-D_CRT_SECURE_NO_DEPRECATE)
     ENDIF(MSVC)
-	IF(USE_INTEL_COMPILER)
-		# Support for the quadruple precision:
-		SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Qoption,cpp,--extended_float_type")
-	ENDIF(USE_INTEL_COMPILER)
 ENDIF(WIN32)
-
-# Check that we have the files we need
-#SET(CMAKE_REQUIRED_INCLUDES ${EIGEN_INCLUDE_PATH})
-#CHECK_INCLUDE_FILE_CXX("Eigen/Core" HAVE_EIGEN_H)
-#SET(CMAKE_REQUIRED_INCLUDES "")
-
-#IF(NOT HAVE_EIGEN_H)
-#    MESSAGE(FATAL_ERROR "[ERROR] Cannot find eigen/Core. Set EIGEN_INCLUDE_PATH")
-#ELSE()
-#    MESSAGE(STATUS "[STATUS] Found eigen/Core")
-#ENDIF()
 
 IF(USE_SWISSTOPO)
 	MESSAGE(STATUS "[STATUS] Swisstopo transformations enabled.")
@@ -74,14 +45,6 @@ ELSE(USE_SWISSTOPO)
 	MESSAGE(STATUS "[STATUS] Swisstopo transformations disabled.")
 ENDIF(USE_SWISSTOPO)
 
-IF(NOT UNIX)
-	IF(NOT EXISTS "${VCREDIST_INSTALLER_PATH}")
-	    MESSAGE(FATAL_ERROR "[ERROR] Cannot find VS redistribuable installer at ${VCREDIST_INSTALLER_PATH}")
-	ELSE()
-	    MESSAGE(STATUS "[STATUS] Found VS redistribuable installer at ${VCREDIST_INSTALLER_PATH}")
-	ENDIF()
-ENDIF()
-
 INCLUDE_DIRECTORIES(${EIGEN_INCLUDE_PATH} ${SURVEYLIB_INCLUDE_PATH})
 	
 IF(CMAKE_PROJECT_NAME STREQUAL "SurveyLib")
@@ -99,25 +62,3 @@ ELSE()
 		LINK_DIRECTORIES ("${add_bindir}")
 	ENDFOREACH(sublib) 
 ENDIF()
-
-
-########################################################################
-# Macro to merge libraries (Obsolete ATM)
-########################################################################
-# MACRO(MERGE_STATIC_LIBS MERGE_ARGS)
-# # create a library from the sub-projects
-# ADD_LIBRARY(${ARGV0} STATIC "${CMAKE_SOURCE_DIR}/empty.cpp")
-
-# SET(OTHERLIBS)
-# FOREACH(newlib ${ARGN})
-	# # Add the sub-library to the list of libraries
-    # SET(OTHERLIBS "${OTHERLIBS}" " ${STAGING_DIRECTORY}/${newlib}.lib" )
-# ENDFOREACH(newlib)
-# # Remove the semicolons from the list
-# STRING(REPLACE ";" " " STR_OTHERLIBS "${OTHERLIBS}")
-# # add the additional lib-files manually to the linker
-# SET_TARGET_PROPERTIES(LSAlgorithmsLibrary PROPERTIES STATIC_LIBRARY_FLAGS ${STR_OTHERLIBS})
-# # let it manually depend on the sub-projects
-# ADD_DEPENDENCIES(${ARGV0} ${ARGN})
-# ENDMACRO(MERGE_STATIC_LIBS)
-########################################################################
