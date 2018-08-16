d8b83d713ea5990641e12f3cfc9d97b907e79b75
Rémi Ducceschi <remi.ducceschi@gmail.com>
2018-08-16 13:45:06 +0200

    Tools: use std::filesystem
    - filesystem method replaced by C++17 standard

--- source/Tools/headers/FileUtils.h
+++ source/Tools/headers/FileUtils.h
@@ -1,57 +1,61 @@
 #ifndef __FILE_UTILS__
 #define __FILE_UTILS__
 
-
-#ifdef __linux__    
-	#define slash '/'
+#ifdef __linux__
+#	define slash '/'
 #else
-	#define slash '\\'
+#	define slash '\\'
 #endif
 
 #include <string>
 
-
 /**
-* File utilities regrouped in the namespace svlTools 
-*/
+ * File utilities regrouped in the namespace svlTools.
+ *
+ * This is just a wrapper around the new C++17 std::filesystem library. Prefer using directly std::filesystem from now.
+ * See https://en.cppreference.com/w/cpp/filesystem
+ */
 
 namespace svlTools
 {
-	/**
-	* Returns the current directory
-	*/
-	std::string getCurrentDirectory();
+/**
+ * Returns the current directory
+ */
+[[deprecated("Use methods from std::filesystem instead.")]]
+std::string getCurrentDirectory();
 
-	/**
-	* Returns the full absolute path and filename, changing the slash symbols when necessary (Windows)
-	* If no absolute path defined in the given filename, it appends the current directory.
-	*
-	* @param fileName Simple filename or complete absolute path
-	*/
-	std::string getPathFileName(std::string fileName);
+/**
+ * Returns the full absolute path and filename, changing the slash symbols when necessary (Windows)
+ * If no absolute path defined in the given filename, it appends the current directory.
+ *
+ * @param fileName Simple filename or complete absolute path
+ */
+[[deprecated("Use methods from std::filesystem instead.")]]
+std::string getPathFileName(const std::string &fileName);
 
-	/**
-	* Windows only method: Changes the directory path -/ become \
-	*
-	* @param path Reference to the path to be modified 
-	*/
-	void changePathDirSlash(std::string& path);
+/**
+ * Changes the directory path separator to std::filesystem::path::preferred_separator.
+ *
+ * @param path Reference to the path to be modified
+ */
+[[deprecated("Use methods from std::filesystem instead.")]]
+void changePathDirSlash(std::string &path);
 
+/**
+ * Returns the filename(or full path + filename) without its possible extension
+ *
+ * @param path Full path or filename
+ */
+[[deprecated("Use methods from std::filesystem instead.")]]
+std::string getFilePathWithoutExtension(const std::string &path);
 
-	/**
-	* Returns the filename(or full path + filename) without its possible extension
-	*
-	* @param path Full path or filename
-	*/
-	std::string getFilePathWithoutExtension(const std::string& path);
-
-	/**
-	* Creates a new output file
-	*
-	* @param outFilePath Full path or filename
-	*/
-	void createOutputFile(const std::string outFilePath);
-}
-
+/**
+ * Creates a new output directory
+ *
+ * @param outFilePath Full path or filename
+ */
+[[deprecated("Use methods from std::filesystem instead.")]]
+void createOutputFile(const std::string outFilePath);
+} // namespace svlTools
 
 #endif // __FILE_UTILS__
--- source/Tools/sources/FileUtils.cpp
+++ source/Tools/sources/FileUtils.cpp
@@ -1,98 +1,47 @@
-#include <algorithm>
-#include <cctype>
-#include <cstdio>
-
 #include "FileUtils.h"
+
+#include <filesystem>
+
 #include "Logger.hpp"
 
-#ifdef __linux__    
-	#include <unistd.h>
-	#define GetCurrentDir getcwd
-	#include <sys/types.h>
-	#include <sys/stat.h>
-	#include <unistd.h>
-	#include <fcntl.h>
-#else
-	#include <direct.h>
-	#define GetCurrentDir _getcwd
-	#include <windows.h>
-#endif
-
-
+namespace fs = std::filesystem;
 
 namespace svlTools
 {
+std::string getCurrentDirectory()
+{
+	return fs::current_path().make_preferred().string();
+}
 
-	/// Returns the current directory
-	std::string getCurrentDirectory() {
+std::string getPathFileName(const std::string &fileName)
+{
+	return fs::weakly_canonical(fs::absolute(fileName)).make_preferred().string();
+}
 
-		char cCurrentPath[FILENAME_MAX];
-		if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
-		{
-			logWarning() << "System could no retrieve the current directory!";
-			return "";
-		}
-		cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
-		logDebug() << "Current directory retrieved by the system: " << cCurrentPath;
-		return std::string(cCurrentPath);
-	}
+void changePathDirSlash(std::string &path)
+{
+	fs::path p(path);
+	path = p.make_preferred().string();
+}
 
-	/// Returns the filename including its absolute path
-	/// If no absolute path defined in the given filename, it appends the current directory
-	std::string getPathFileName(std::string fileName)
+std::string getFilePathWithoutExtension(const std::string &path)
+{
+	const fs::path p(path);
+	return (p.parent_path() / p.stem()).make_preferred().string();
+}
+
+void createOutputFile(const std::string outFilePath)
+{
+	fs::path p(outFilePath);
+	if (p.has_filename())
+		p = p.parent_path();
+	try
 	{
-		std::string filePath = "";
-#ifdef __linux__    
-		if (fileName[0] != slash)
-			filePath = svlTools::getCurrentDirectory() + slash + fileName;
-		else
-			filePath = fileName;
-#else
-		svlTools::changePathDirSlash(fileName);
-
-		if ((std::isalpha(fileName[0]) && fileName[1] == ':' && fileName[2] == slash) || fileName[0] == slash)
-			filePath = fileName;
-		else
-			filePath = svlTools::getCurrentDirectory() + slash + fileName;
-#endif
-		return filePath;
+		fs::create_directories(p);
 	}
-
-	/// Windows only method
-	/// Change the path directory path -/ become \ (for windows)
-	void changePathDirSlash(std::string& path)
+	catch (const fs::filesystem_error &e)
 	{
-#ifndef __linux__
-		std::transform(std::begin(path), std::end(path), std::begin(path), [](char c) -> char { return c == '/' ? slash : c; });
-#endif // !__linux__
+		logWarning() << "Output directory does not exist and could not be created :'" << e.what() << "'";
 	}
-
-	/// Returns the filename (or full path+filename) without the possible extension
-	std::string getFilePathWithoutExtension(const std::string& path)
-	{
-		std::size_t found = path.find_last_of(".");
-		return path.substr(0, found);
-	}
-
-	/// Create the output file
-	void createOutputFile(const std::string outFilePath)
-	{
-		std::string outputFileDirectory = outFilePath.substr(0, outFilePath.find_last_of(slash));
-		//If does not exist, needs to be created
-#ifdef __linux__
-		struct stat st = { 0 };
-		if (stat(outputFileDirectory.c_str(), &st) == -1)
-			if (creat(outputFileDirectory.c_str(), S_IRWXU | S_IRWXG) == -1) {
-				logWarning() << "Output directory does not exist and could not be created";
-			}
-#else
-		CreateFile(outputFileDirectory.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-		DWORD ftyp = GetFileAttributesA(outputFileDirectory.c_str());
-		if (ftyp == INVALID_FILE_ATTRIBUTES) { //This means that the directory still does not exist, using default one
-			logWarning() << "Output directory does not exist and could not be created";
-		}
-#endif
-	}
-
+}
 } // namespace svlTools
