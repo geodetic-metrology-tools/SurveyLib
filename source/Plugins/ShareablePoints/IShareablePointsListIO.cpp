33152347a0e18a488c4c2e78502f5851b4d62e54
Rémi Ducceschi <remi.ducceschi@gmail.com>
2018-08-24 14:49:30 +0200

    Plugins: update IO for list of points
    - work on strings instead of files

--- source/Plugins/ShareablePoints/IShareablePointsListIO.cpp
+++ source/Plugins/ShareablePoints/IShareablePointsListIO.cpp
@@ -4,40 +4,57 @@
 
 #include "Logger.hpp"
 
- std::ifstream IShareablePointsListIO::openRead(bool binary)
+ std::ifstream IShareablePointsListIO::openRead(const std::string& filename, bool binary)
  {
-	 if (_filename.empty())
+	 if (filename.empty())
 	 {
 		 logDebug() << "No file name given, can't open.";
-		 throw SPIOException("No file name given, can't open.", _filename);
+		 throw SPIOException("No file name given, can't open.");
 	 }
 	 std::ifstream::openmode openmode{};
 	 if (binary)
 		 openmode |= std::ifstream::binary;
-	 std::ifstream file(_filename, openmode);
+	 std::ifstream file(filename, openmode);
 	 if (!file)
 	 {
-		 logDebug() << "Can't open file" << _filename << ".";
-		 throw SPIOException("Can't open file" + _filename + ".", _filename);
+		 logDebug() << "Can't open file" << filename << ".";
+		 throw SPIOException("Can't open file.", filename);
 	 }
 	 return file;
  }
 
- std::ofstream IShareablePointsListIO::openWrite(bool binary)
+ std::ofstream IShareablePointsListIO::openWrite(const std::string& filename, bool binary)
  {
-	 if (_filename.empty())
+	 if (filename.empty())
 	 {
 		 logDebug() << "No file name given, can't open.";
-		 throw SPIOException("No file name given, can't open.", _filename);
+		 throw SPIOException("No file name given, can't open.");
 	 }
 	 std::ofstream::openmode openmode = std::ofstream::trunc;
 	 if (binary)
 		 openmode |= std::ofstream::binary;
-	 std::ofstream file(_filename, openmode);
+	 std::ofstream file(filename, openmode);
 	 if (!file)
 	 {
-		 logDebug() << "Can't open file" << _filename << ".";
-		 throw SPIOException("Can't open file" + _filename + ".", _filename);
+		 logDebug() << "Can't open file" << filename << ".";
+		 throw SPIOException("Can't open file.", filename);
 	 }
 	 return file;
+ }
+
+ std::string IShareablePointsListIO::readFile(const std::string & filename, bool binary)
+ {
+	 auto file = openRead(filename, binary);
+	 auto fbuf = file.rdbuf();
+	 const std::streampos size = fbuf->pubseekoff(0, file.end);
+	 fbuf->pubseekoff(0, file.beg);
+	 auto contents = std::make_unique<char[]>(size);
+	 fbuf->sgetn(contents.get(), size);
+	 return std::string(contents.get(), size);
+ }
+
+ void IShareablePointsListIO::writeFile(const std::string & filename, const std::string & contents, bool binary)
+ {
+	 auto file = openWrite(filename, binary);
+	 file.write(contents.c_str(), contents.size());
  }
