c28066689b243c8f2de643d65ebef88321f59820
Przemyslaw Lewandowski <przemyslaw.lewandowski@cern.ch>
2022-12-09 11:00:30 +0100

    temp

--- Init.cmake
+++ Init.cmake
@@ -7,12 +7,16 @@ if(GIT_FOUND)
     set(GIT_EXE ${GIT_EXECUTABLE})
 endif(GIT_FOUND)
 
-if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/deps/SUSoftCMakeCommon/.git")
-    message("Initializing SUSoftCMakeCommon submodule")
-    execute_process(COMMAND ${GIT_EXE} "submodule" "update" "--init" "--recursive" "deps/SUSoftCMakeCommon"
+if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/SUSoftCMakeCommon/.git")
+    message("Initializing cmake submodule")
+    execute_process(COMMAND ${GIT_EXE} "submodule" "update" "--init" "--recursive" "cmake"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
 endif()
 
-include("${CMAKE_CURRENT_SOURCE_DIR}/deps/SUSoftCMakeCommon/CMakeLists.txt")
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/SUSoftCMakeCommon;${CMAKE_CURRENT_SOURCE_DIR}/SUSoftCMakeCommon/cmake")
 
 include(PostInit)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/SUSoftCMakeCommon/CMakeLists.txt)
+
