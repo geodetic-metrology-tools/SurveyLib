2e188fa4487477f772ec5155bd32c3d356efb403
Vasileios Vlachakis <vasileios.vlachakis@cern.ch>
2023-02-24 18:06:19 +0100

    create lists in TAdjustableHelmertTransformation serialization

--- source/LSAlgorithms/sources/AdjustableObjects/TAdjustableHelmertTransformation.cpp
+++ source/LSAlgorithms/sources/AdjustableObjects/TAdjustableHelmertTransformation.cpp
@@ -585,19 +585,30 @@ void TAdjustableHelmertTransformation::serialize(SerializerObject::Serialization
 			fEstParameter.omega.getRadiansValue(),
 			fEstParameter.phi.getRadiansValue(),
 			fEstParameter.scale,
-	});                                  
-	obj.addProperty("fEstPrecisionRotation", fEstPrecisionRotation->getRadiansValue());
-	obj.addProperty("fEstPrecisionScale", fEstPrecisionScale);
-	obj.addProperty("fEstPrecisionTranslation", fEstPrecisionTranslation->getMetresValue());
-	// TODO?
-	obj.addProperty("fixedRotationsK", fixedRotations[0]);
-	obj.addProperty("fixedRotationsO", fixedRotations[1]);
-	obj.addProperty("fixedRotationsP", fixedRotations[2]);
-	obj.addProperty("fixedScale", fixedScale[0]);
+	});
+	
+	obj.addProperty("fEstPrecision",
+		std::vector<double>{
+			fEstPrecisionTranslation[0].getMetresValue(),
+			fEstPrecisionTranslation[1].getMetresValue(),
+			fEstPrecisionTranslation[2].getMetresValue(),
+			fEstPrecisionRotation[0].getRadiansValue(),
+			fEstPrecisionRotation[1].getRadiansValue(),
+			fEstPrecisionRotation[2].getRadiansValue(),
+			fEstPrecisionScale,
+		});
 
-	obj.addProperty("fixedTranslationsX", fixedTranslations[0]);
-	obj.addProperty("fixedTranslationsY", fixedTranslations[1]);
-	obj.addProperty("fixedTranslationsZ", fixedTranslations[2]);
+	obj.addProperty("fixedTranfParam",
+		std::vector<bool>{
+			(bool)fixedTranslations[0],
+			(bool)fixedTranslations[1],
+			(bool)fixedTranslations[2],
+			(bool)fixedRotations[0],
+			(bool)fixedRotations[1],
+			(bool)fixedRotations[2],
+			(bool)fixedScale[0],
+	});
+
 	obj.addProperty("fProvParameter",
 		std::vector<double>{
 			fProvParameter.tX.getMetresValue(),
@@ -607,16 +618,32 @@ void TAdjustableHelmertTransformation::serialize(SerializerObject::Serialization
 			fProvParameter.omega.getRadiansValue(),
 			fProvParameter.phi.getRadiansValue(),
 			fProvParameter.scale,
-		});    
-	obj.addProperty("fRotStandDev", fRotStandDev);
-	obj.addProperty("fScaleStandDev", fScaleStandDev);
-	obj.addProperty("fTransStandDev", fTransStandDev);
+		});
+
+	obj.addProperty("fTranfParamStandDev",
+		std::vector<double>{
+			fTransStandDev[0].getMetresValue(),
+			fTransStandDev[1].getMetresValue(),
+			fTransStandDev[2].getMetresValue(),
+			fRotStandDev[0].getRadiansValue(),
+			fRotStandDev[1].getRadiansValue(),
+			fRotStandDev[2].getRadiansValue(),
+			fScaleStandDev,
+		});
 
 	obj.addProperty("line", line);
 	obj.addProperty("name", name);
-	obj.addProperty("uidx_rot", uidx_rot);
-	obj.addProperty("uidx_scale", uidx_scale);
-	obj.addProperty("uidx_trans", uidx_trans);
+
+	obj.addProperty("uidx",
+		std::vector<int>{
+			uidx_trans[0],
+			uidx_trans[1],
+			uidx_trans[2],
+			uidx_rot[0],
+			uidx_rot[1],
+			uidx_rot[2],
+			uidx_scale,
+		});
 }
 #endif // USE_SERIALIZER
 
