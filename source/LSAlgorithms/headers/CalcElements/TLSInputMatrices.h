6e07403938a015bb9a423ccef7712201424ae485
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-06-15 17:33:23 +0200

    masked e and o indices struct

--- source/LSAlgorithms/headers/CalcElements/TLSInputMatrices.h
+++ source/LSAlgorithms/headers/CalcElements/TLSInputMatrices.h
@@ -147,7 +147,12 @@ public:
 	//@}
 
 	// can be used to mask rows of the A matrix during adjustment
-	std::set<int> maskedIndices;
+		struct
+		{
+			std::set<int> EIndices;
+			std::set<int> OIndices;
+		}
+		maskData;
 
 	// mult from right to mask columns
 	const TSparseMatrix getRightFactor();
