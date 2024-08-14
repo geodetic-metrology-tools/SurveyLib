faa5cc6979cb373194f904c6192d82598377373e
Dominik Wassmer <dominik.wassmercern.ch>
2024-08-14 10:58:01 +0200

    Refining the SimpleTimer from feedback and adding a test

---
+++ source/Tests/Tools/testSimpleTimer.cpp
@@ -0,0 +1,51 @@
+#include <tut/tut.hpp>
+
+#include <thread>
+
+#include "SimpleTimer.h"
+
+namespace tut
+{
+struct toolsSimpleTimer
+{
+};
+
+typedef test_group<toolsSimpleTimer> tst;
+tst tools_stringmanager_group("Test Tools - SimpleTimer package.");
+typedef tst::object testobject;
+} // namespace tut
+
+namespace tut
+{
+template<>
+template<>
+void testobject::test<1>()
+{
+	set_test_name("SimpleTimer: test start stop");
+
+	SimpleTimer timer;
+	timer.start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	size_t duration = timer.stop();
+	ensure_distance(duration, (size_t)200, (size_t)100);
+}
+
+template<>
+template<>
+void testobject::test<2>()
+{
+	set_test_name("SimpleTimer: test start step stop");
+
+	SimpleTimer timer;
+	timer.start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	timer.step();
+	std::this_thread::sleep_for(std::chrono::milliseconds(300));
+	timer.step();
+	timer.stop();
+	timer.printSteps();
+	auto steps = timer.getSteps();
+	ensure_distance(steps[0], (size_t)200, (size_t)100);
+	ensure_distance(steps[1], (size_t)500, (size_t)100);
+}
+}
--- source/Tools/headers/SimpleTimer.h
+++ source/Tools/headers/SimpleTimer.h
@@ -5,28 +5,48 @@
 #include <vector>
 #include <sstream>
 
-template<class T>
-class basicSimpleTimer {
+template<class T = std::chrono::milliseconds>
+class SimpleTimer {
 public:
-	basicSimpleTimer() = default;
-	~basicSimpleTimer() = default;
-	void start() { m_startPoint = std::chrono::high_resolution_clock::now(); }
+	SimpleTimer() = default;
+	~SimpleTimer() = default;
+	void start() { m_startPoint = std::chrono::high_resolution_clock::now(); } 
+	/**
+	* Records the time into steps. The recorded time is always relative to when the timer was started
+	* and NOT compared to the previous step. 
+	*/
 	void step() { m_steps.emplace_back(std::chrono::duration_cast<T>(std::chrono::high_resolution_clock::now() - m_startPoint).count()); }
-	void stop() 
+	[[maybe_unused]] size_t stop() 
 	{
 		auto measuredTime = std::chrono::duration_cast<T>(std::chrono::high_resolution_clock::now() - m_startPoint).count();
-		std::cout << "Elapsed Time in milliseconds: " << measuredTime << "ms" << std::endl;
+		printStop(measuredTime);
+		return measuredTime;
 	}
-	std::string printSteps() 
+	void printSteps() 
 	{ 
-		std::stringstream output("[");
-		for (int i = 0; i < m_steps.size(); i++)
+		if (!m_steps.size())
+		{
+			std::cout << "[]" << std::endl;
+			return;
+		}
+		std::stringstream output;
+		output << "[";
+		for (int i = 0; i < m_steps.size() - 1; i++)
 		{
 			output << m_steps[i].count() << "ms, ";
 		}
-		output << "]";
+		output << m_steps[m_steps.size() - 1].count() << "ms]";
 		std::cout << output.str() << std::endl;
-		return output.str();
+	}
+	std::vector<size_t> getSteps()
+	{
+		std::vector<size_t> result;
+		result.reserve(m_steps.size());
+		for (auto step : m_steps)
+		{
+			result.emplace_back(step.count());
+		}
+		return result;
 	}
 	void reset()
 	{
@@ -35,7 +55,18 @@ public:
 	}
 	std::vector<T> m_steps;
 private:
+	void printStop(size_t duration) {std::cout << "Elapsed Time: " << duration << std::endl;}
 	std::chrono::steady_clock::time_point m_startPoint;
 };
 
-#define SimpleTimer basicSimpleTimer<std::chrono::milliseconds>
+template<>
+void SimpleTimer<std::chrono::milliseconds>::printStop(size_t duration)
+{
+	std::cout << "Elapsed Time in milliseconds: " << duration << "ms" << std::endl;
+}
+
+template<>
+void SimpleTimer<std::chrono::seconds>::printStop(size_t duration)
+{
+	std::cout << "Elapsed Time in seconds: " << duration << "s" << std::endl;
+}
