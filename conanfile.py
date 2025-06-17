4e86d74a1d0bb2d09b730eb81b6f3171b5a178a7
Rishabh Singh <rishabh.singh@cern.ch>
2025-06-17 16:17:58 +0200

    test .lock file for dependency scanning

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+
+class SurveyLibConan(ConanFile):
+    name = "surveylib"
+    version = "4.3.1"
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    exports_sources = "source/*", "SurveryLibVersion.in", "lib/*", "licenses/*"
+
+    requires = (
+        "eigen/3.4.0",
+    )
+
+    default_options = {
+        "eigen/*:build_tests": False,
+    }
+
+    def layout(self):
+        self.folders.source = "source"
+        self.folders.build = "build"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["surveylib"]
