b458c32e67592cf38c8eca461f6cf186ce534c80
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2025-06-25 17:14:26 +0200

    access also fixed pt vars

--- source/LSAlgorithms/sources/AdjustableObjects/TAdjustablePoint.cpp
+++ source/LSAlgorithms/sources/AdjustableObjects/TAdjustablePoint.cpp
@@ -441,6 +441,20 @@ void TAdjustablePoint::setValue(int idx, TReal value)
 	throw std::logic_error("Invalid unknown index in parameter access. Point " + getName());
 }
 
+void TAdjustablePoint::setEstVal(int idx, TReal value)
+{
+	switch (idx)
+	{
+	case 0:
+		fEstimatedValue.setX(TLength(value));
+	case 1:
+		fEstimatedValue.setY(TLength(value));
+	case 2:
+		fEstimatedValue.setZ(TLength(value));
+	}
+}
+
+
 /*! Gets the euclidean estimated distance from this adjustable point to another one.
 	\param[in] Second adjustable point
 */
