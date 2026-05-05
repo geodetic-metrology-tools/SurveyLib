f35f9f50d0e76b29fb850743480a72332ff8110b
Benjamin Weyer <benjamin.weyer@cern.ch>
2026-05-05 10:55:56 +0200

    remove hardcoded SRS

--- source/SpatialObjects/headers/ReferenceSurfaces/TRegionalGeoid.h
+++ source/SpatialObjects/headers/ReferenceSurfaces/TRegionalGeoid.h
@@ -39,7 +39,13 @@ public:
 	TRegionalGeoid();
 
 	/// cconstructor taking the name, the definition reference frame and ellipsoid, the calculation reference frame and the path to the geoid grid file
-	TRegionalGeoid(const std::string &name, const TRefSystemFactory::EGeoid &geoidId, TAReferenceFrame *def, TReferenceEllipsoid *ell, TAReferenceFrame *calc, const std::string &pathToFile);
+	TRegionalGeoid(const std::string &name,
+		const TRefSystemFactory::EGeoid &geoidId,
+		TAReferenceFrame *def,
+		TReferenceEllipsoid *ell,
+		TAReferenceFrame *calc,
+		const std::string &pathToFile,
+		const int &epsgCode = 4345);
 
 	/// Destructor
 	virtual ~TRegionalGeoid();
@@ -71,7 +77,15 @@ private:
 
 	bool prepareXiAndEtaComputation(const TSpatialPosition &sp, TReal &deltaN, TAngle &lambdaOrPhi, TAngle &gridSpacing, const std::string &xiOrEta) const;
 
-	bool getNatCornerAroundPoint(const TReal &xPoint, const TReal &yPoint, const GDALDataset *dataset, const TReal &gridSpacingX,  const TReal &gridSpacingY, TReal &n_XBefore_Y, TReal &n_XAfter_Y, TReal &n_X_YBefore, TReal &n_X_YAfter) const;
+	bool getNatCornerAroundPoint(const TReal &xPoint,
+		const TReal &yPoint,
+		const GDALDataset *dataset,
+		const TReal &gridSpacingX,
+		const TReal &gridSpacingY,
+		TReal &n_XBefore_Y,
+		TReal &n_XAfter_Y,
+		TReal &n_X_YBefore,
+		TReal &n_X_YAfter) const;
 
 	/*!Generate message for the NotInGeoidGridException*/
 	std::stringstream generateNotInGeoidGridMessage(const std::string &functionCalled, const TSpatialPosition &position) const;
@@ -81,6 +95,8 @@ private:
 	/**@name Member Attributes */
 	//@{
 	std::string fPathToFile; /*!< path to the geoid grid file */
+
+	int fEPSGCode = 4345; /*!< EPSG code of the geoid grid. 4345 (ETRS89 3D geographic) is default value  */
 	//@}
 };
 
