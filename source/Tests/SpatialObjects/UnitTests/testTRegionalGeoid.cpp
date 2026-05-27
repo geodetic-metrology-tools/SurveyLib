f54308e579a4270ed6df01f2dcd83e5e87bbdefc
Benjamin Weyer <benjamin.weyer@cern.ch>
2026-05-27 09:16:06 +0200

    continue test

--- source/Tests/SpatialObjects/UnitTests/testTRegionalGeoid.cpp
+++ source/Tests/SpatialObjects/UnitTests/testTRegionalGeoid.cpp
@@ -160,6 +160,7 @@ template<>
 template<>
 void object::test<6>()
 {
+	tut::skip();
 	set_test_name("Test for the whole dataset");
 	std::ifstream infile("C:\\Users\\bweyer\\Downloads\\GeocentricDoVControl.txt");
 	std::ofstream outfile("C:\\Users\\bweyer\\Downloads\\GeocentricDoVControl_withModel.txt");
@@ -269,6 +270,9 @@ void object::test<8>()
 	TPositionVector p(x, y, z, TCoordSysFactory::k3DCartesian);
 	TSpatialPosition position(TRefFrameInfo::getReferenceFrame(TRefSystemFactory::kWGS84_G2139), p);
 
+	std::cout << "N: " << AUSGeoid2020->getN(position).getMetresValue() << "\n";
+	std::cout << "Eta: " << AUSGeoid2020->getEta(position).getSecondsValue() << "\n";
+	std::cout << "Xi: " << AUSGeoid2020->getXi(position).getSecondsValue() << "\n";
 	ensure_equals("N", AUSGeoid2020->getN(position).getMetresValue(), 19.727, tolHeight);
 	ensure_equals("Eta", AUSGeoid2020->getEta(position).getSecondsValue(), -6.07, tolDoV);
 	ensure_equals("Xi", AUSGeoid2020->getXi(position).getSecondsValue(), -20.21, tolDoV);
@@ -318,9 +322,144 @@ void object::test<10>()
 	TSpatialPosition position(TRefFrameInfo::getReferenceFrame(TRefSystemFactory::kWGS84_G2139), p);
 
 	//ensure_equals("N", GEOID18->getN(position).getMetresValue(), 19.727, tolHeight);
+	std::cout << "Eta: " << GEOID18->getEta(position).getSecondsValue() << "\n";
+	std::cout << "Xi: " << GEOID18->getXi(position).getSecondsValue() << "\n";
 	ensure_equals("Eta", GEOID18->getEta(position).getSecondsValue(), 1.32, tolDoV);
 	ensure_equals("Xi", GEOID18->getXi(position).getSecondsValue(), 2.88, tolDoV);
+
+	// See https://geodesy.noaa.gov/library/pdfs/NOAA_TR_NOS_NGS_0072.pdf
+	// and https://ia801303.us.archive.org/31/items/heiskanen_morits_1967_physical_geodesy/heiskanen_morits_1967_physical_geodesy.pdf
 }
+
+template<>
+template<>
+void object::test<11>()
+{
+	set_test_name("Test compared to ICGEM");
+	std::ifstream infile("C:\\Users\\bweyer\\cernbox\\Documents\\Development\\SurveyLib\\ImplementationGeoid\\DoVObservations.txt");
+	std::ofstream outfile("C:\\Users\\bweyer\\cernbox\\Documents\\Development\\SurveyLib\\ImplementationGeoid\\DoVObservations_withModelCppNegCorrection.txt");
+
+	if (!infile.is_open())
+	{
+		std::cerr << "Error opening input file\n";
+	}
+
+	if (!outfile.is_open())
+	{
+		std::cerr << "Error opening output file\n";
+	}
+
+	std::string line;
+
+	// Skip header
+	std::getline(infile, line);
+
+	// Write output header
+	outfile << "Point\tLongitude\tLatitude\th\tEta\tXi\tEtaModel\tXiModel\tDeltaEta\tDeltaXi\n";
+	outfile << std::fixed << std::setprecision(6);
+
+	// Initialize geoid model once
+	//TAReferenceFrame *CHTRF95(TRefSystemFactory::getRefSystemFactory()->getRefFrame(TRefSystemFactory::kCHTRF95));
+	//TRegionalGeoid *FCC_G2025 = new TRegionalGeoid("CHGeo2004_ETRS", TRefSystemFactory::EGeoid::kCHGeo2004_ETRS, CHTRF95,
+	//	TRefSystemFactory::getRefSystemFactory()->getEllipsoid(TRefSystemFactory::kGRS80), CHTRF95,
+	//	"C:\\Users\\bweyer\\cernbox\\Documents\\FCC\\Geoid\\JuliaComparison\\Julia\\FCC-G2025_V1.0.tif");
+	const double tolHeight = 1e-3;
+	const double tolDoV = 1e-2;
+	std::string pathToFile("C:\\Users\\bweyer\\cernbox\\Documents\\Development\\SurveyLib\\ImplementationGeoid\\geoid_EIGEN-6C4_AOI.tiff");
+	// Initialize geoid model once
+	TAReferenceFrame *WGS84(TRefSystemFactory::getRefSystemFactory()->getRefFrame(TRefSystemFactory::kWGS84_G2139));
+	TRegionalGeoid *EIGEN = new TRegionalGeoid("EIGEN", TRefSystemFactory::EGeoid::kCHGeo2004_ETRS, WGS84,
+		TRefSystemFactory::getRefSystemFactory()->getEllipsoid(TRefSystemFactory::kGRS80), WGS84, pathToFile, 4326, GRIORA_Cubic);
+
+
+	while (std::getline(infile, line))
+	{
+		if (line.empty())
+			continue;
+
+		std::istringstream iss(line);
+
+		int point;
+		double X, Y, Z;
+		double Eta, Xi;
+
+		iss >> point >> X >> Y >> Z >> Eta >> Xi;
+
+		// Build position
+		TAngle lambda(X, TAngle::kDeciDegs);
+		TAngle phi(Y, TAngle::kDeciDegs);
+		TLength h(Z);
+		TPositionVector pv(TCoordSysFactory::kGeodetic);
+		pv.setPhiEllipsoid(phi);
+		pv.setLambdaEllipsoid(lambda);
+		pv.setH(h);
+		
+		TSpatialPosition position(TRefFrameInfo::getReferenceFrame(TRefSystemFactory::kWGS84_G2139));
+		position.setCoordinates(pv);
+
+		// Compute model values
+		double EtaModel = EIGEN->getEta(position).getSecondsValue();
+		double XiModel = EIGEN->getXi(position).getSecondsValue();
+
+		// Differences
+		double deltaEta = EtaModel - Eta;
+		double deltaXi = XiModel - Xi;
+
+		// Write results
+		outfile << point << "\t" << lambda.getDeciDegsValue() << "\t" << phi.getDeciDegsValue() << "\t" << h << "\t" << Eta << "\t" << Xi << "\t" << EtaModel << "\t"
+				<< XiModel << "\t"
+				<< deltaEta << "\t" << deltaXi << "\n";
+	}
+
+	infile.close();
+	outfile.close();
+
+	std::cout << "Processing completed.\n";
+}
+
+template<>
+template<>
+void object::test<12>()
+{
+/*
+Name	ESSERTINES
+Country	CH
+Year of measurement	1996
+Instrument	TZK3
+Xi (CH1903) [arcseconds]	-13.09
+Eta (CH1903) [arcseconds]	7.32
+Xi (ETRS89) [arcseconds]	-8.5
+Eta (ETRS89) [arcseconds]	9.23
+Type of measurement station	Analoge Zenitkamera (1980-1996)
+Approx WGS84~ETRF93 coordinates (lat, long, altitude):	46.599584, 7.239956 921 m
+	*/
+
+		set_test_name("CHGEO with true value");
+TAngle lambda(7.239956, TAngle::kDeciDegs);
+		TAngle phi(46.599584, TAngle::kDeciDegs);
+TLength h(921+51.1);
+	TPositionVector pv(TCoordSysFactory::kGeodetic);
+	pv.setPhiEllipsoid(phi);
+	pv.setLambdaEllipsoid(lambda);
+	pv.setH(h);
+
+	TSpatialPosition position(TRefFrameInfo::getReferenceFrame(TRefSystemFactory::kCHTRF95));
+	position.setCoordinates(pv);
+
+	std::string pathToFile("C:\\Users\\bweyer\\Downloads\\Geoid_OGD\\chgeo2004_ETRS.agr");
+	// Initialize geoid model once
+	TAReferenceFrame *chtrf95(TRefSystemFactory::getRefSystemFactory()->getRefFrame(TRefSystemFactory::kCHTRF95));
+	TRegionalGeoid *pCHGeo2004_ETRS = new TRegionalGeoid("CHGeo2004_ETRS", TRefSystemFactory::EGeoid::kCHGeo2004_ETRS, chtrf95,
+		TRefSystemFactory::getRefSystemFactory()->getEllipsoid(TRefSystemFactory::kGRS80), chtrf95, pathToFile);
+	
+
+//		TAGeoidModel *geoidModel(TRefSystemFactory::getRefSystemFactory()->getGeoid(TRefSystemFactory::kCHGeo2004_ETRS));
+	std::cout << "Xi: " << pCHGeo2004_ETRS->getXi(position).getSecondsValue() << "\n";
+	std::cout << "Eta: " << pCHGeo2004_ETRS->getEta(position).getSecondsValue() << "\n";
+	std::cout << "N: " << pCHGeo2004_ETRS->getN(position).getMetresValue() << "\n";
+	ensure_equals("Eta", pCHGeo2004_ETRS->getEta(position).getSecondsValue(), 9.23);
+	ensure_equals("Xi", pCHGeo2004_ETRS->getXi(position).getSecondsValue(), -8.5);
+	}
 
 
 } // namespace tut
